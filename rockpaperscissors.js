27cc594ff59903ed2caf4cc639b885b913db02fb
Shommon <sdtai2@outlook.com>
2023-11-03 00:02:37 -0400

    modified script.js Changed playstyle to roundplay with score tracking

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,28 +1,64 @@
 const choices = ['ROCK','PAPER','SCISSORS'];
 const draw = 'Its a DRAW!'
 const playerWin = 'Player WINS!'
-const ComputerWin = 'Computer WINS!'
+const computerWin = 'Computer WINS!'
 const playerText = 'Player Chose: '
 const computerText = 'Computer Chose: '
+const numrounds = 5;
 
-let getComputerChoice = function(){
-	return choices[Math.floor(Math.random()*choices.length)];
-};
+let playerPrompt = () => prompt('Rock, Paper, or Scissors?');
 
 
-let playRound = function(player, computer){
-	let playerChoice = player.toString().toUpperCase()
-	let choiceText = playerText + playerChoice + ', '+ computerText + computer + ", "
-	if (playerChoice == computer){
-		return console.log(choiceText + draw);
-	} else if (playerChoice == 'ROCK' && computer == 'SCISSORS'){
-			return console.log(choiceText + playerWin);
-	} else if (playerChoice == 'PAPER' && computer == 'ROCK'){
-			return console.log(choiceText + playerWin);
-	} else if (playerChoice == 'SCISSORS' && computer == 'PAPER'){
-			return console.log(choiceText + playerWin);
+//Take in player choice and computer choice
+function winCondition(player, computer){
+	if (player == computer){
+		return draw;
+	} else if (player == 'ROCK' && computer == 'SCISSORS'){
+			return playerWin;
+	} else if (player == 'PAPER' && computer == 'ROCK'){
+			return playerWin;
+	} else if (player == 'SCISSORS' && computer == 'PAPER'){
+			return playerWin;
 	} else {
-			return console.log(choiceText + ComputerWin);
+			return computerWin;
 	}
-	return winner
 };
+
+function roundPlay(rounds){
+	let score = [0,0];
+	for (i=0; i <= rounds; i++){
+		//Convert strings to uppercase and choosing computer choice
+		let playerChoice = playerPrompt().toString().toUpperCase()
+		let computerChoice = choices[Math.floor(Math.random()*choices.length)];
+		let choiceText = playerText + playerChoice + ', '+ computerText + computerChoice + ", ";
+		
+		if (score[0] == 3){
+		console.log('PLAYER WINS!!!!');
+		} else if (score[1] == 3){
+			console.log('COMPUTER WINS!!!!');
+		} else if (score[0] < 3 || score[1] < 3){
+
+				if (winCondition(playerChoice,computerChoice) == playerWin){
+				score[0] +=1;
+				console.log(choiceText+ winCondition(playerChoice, computerChoice));
+				console.log(score)
+
+			} else if (winCondition(playerChoice,computerChoice) == draw){
+				console.log(choiceText+ winCondition(playerChoice, computerChoice));
+				console.log(score)
+			} else {
+				score[1] +=1
+				console.log(choiceText+ winCondition(playerChoice, computerChoice))
+				console.log(score)
+			}
+		
+		}
+	}	
+}
+
+
+105
+
+// //Decoration and Outputting Results
+// let choiceText = playerText + playerChoice + ', '+ computerText + computerChoice + ", ";
+// console.log(choiceText+ winCondition(playerChoice, computerChoice))
